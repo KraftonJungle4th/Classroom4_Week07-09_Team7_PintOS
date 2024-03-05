cdfaeafa332a9aa15783a507bce5ce91c3ab0850
pongpongie <arevolvera@gmail.com>
2024-03-05 22:42:56 +0900

    mod thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -629,8 +629,6 @@ init_thread(struct thread *t, const char *name, int priority)
 	t->tf.rsp = (uint64_t)t + PGSIZE - sizeof(void *);
 	t->priority = priority;
 	t->magic = THREAD_MAGIC;
-
-	t->sleep = 0;
 }
 
 /* Chooses and returns the next thread to be scheduled.  Should
